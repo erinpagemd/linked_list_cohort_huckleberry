55c0112f4ea0a852ba0154297d2771ef52c0fd07
YOUR NAME <erinpagemd@yahoo.com>
2015-04-13 16:10:19 -0500

    wrote next_item method and raised ArgumentError

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,8 +1,17 @@
 class LinkedListItem
   include Comparable
-  attr_reader :payload
 
+  attr_reader :payload
   def initialize(data)
     @payload = data
+  end
+
+  attr_reader :next_item
+  def next_item=(value)
+    if value === self
+      raise ArgumentError
+    else
+      @next_item = value
+    end
   end
 end
